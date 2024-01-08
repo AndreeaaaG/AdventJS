ca3beacf62d2ff52bc9d31a56e0d9683180636d7
AndreeaaaG <andreea.gavrila@epg.ro>
2024-01-08 14:38:18 +0200

    Add Day11 folder

---
+++ day11.js
@@ -0,0 +1,39 @@
+function getIndexsForPalindrome(word) {
+    const size = word.length
+  
+    function isPalindrome(word){
+      return word === word.split('').reverse().join('')
+    }
+  
+    if (isPalindrome(word)) {
+      return []
+    }
+  
+    for(let i = 0; i < size; i++){
+      for(let j = i + 1; j < size; j++){
+  
+        // const newWord = word.split('')
+        // const temp = newWord[i]
+        // newWord[i] = newWord[j]
+        // newWord[j] = temp
+  
+        // if(isPalindrome(newWord.join(''))) {
+        //   return [i, j]
+        // }
+  
+        const newWord = 
+          word.substring(0, i) +
+          word[j] +
+          word.substring(i + 1, j) +
+          word[i] + 
+          word.substring(j + 1);
+  
+        if(isPalindrome(newWord)) {
+          return [i, j];
+        }
+  
+      }
+    }
+  
+    return null
+  }
