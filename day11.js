e9d5bbdbc19657fc1dd4144fc325d2d4da60f7c6
AndreeaaaG <andreea.gavrila@epg.ro>
2024-01-09 11:52:46 +0200

    Add tests

--- day11.js
+++ day11.js
@@ -37,3 +37,11 @@ function getIndexsForPalindrome(word) {
   
     return null
   }
+
+
+console.log(getIndexsForPalindrome('anna')) // []
+console.log(getIndexsForPalindrome('abab')) // [0, 1]
+console.log(getIndexsForPalindrome('abac')) // null
+console.log(getIndexsForPalindrome('aaaaaaaa')) // []
+console.log(getIndexsForPalindrome('aaababa')) // [1, 3]
+console.log(getIndexsForPalindrome('caababa')) // null
