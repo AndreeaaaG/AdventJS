7b80da2fbc87db1221e451b27a7fe0edf2904c4a
AndreeaaaG <andreea.gavrila@epg.ro>
2024-01-09 11:54:13 +0200

    Add tests

--- day12.js
+++ day12.js
@@ -11,3 +11,17 @@ function checkIsValidCopy(original, copy) {
     }
     return true
   }
+
+console.log(checkIsValidCopy(
+    'Santa Claus is coming',
+    'sa#ta Cl#us i+ comin#'
+  )) // true
+
+  console.log(checkIsValidCopy(
+    's#nta Cla#s is coming',
+    'p#nt: cla#s #s c+min#'
+  )) // false (due to the initial p)
+
+  console.log(checkIsValidCopy('Santa Claus', 's#+:. c:. s')) // true
+
+  console.log(checkIsValidCopy('Santa Claus', 's#+:.#c:. s')) // false (there is a # where it shouldn't be)
