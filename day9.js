c1db45469a41f54da0ed1aa584fb5174b83ff35f
AndreeaaaG <andreea.gavrila@epg.ro>
2024-01-09 11:49:20 +0200

    Add tests

--- day9.js
+++ day9.js
@@ -1,4 +1,4 @@
-export function adjustLights(lights) {
+function adjustLights(lights) {
     let option1 = 0
     const lightsCopy1 = [...lights]
     const lightsCopy2 = [...lights]
@@ -20,3 +20,18 @@ export function adjustLights(lights) {
   
     return Math.min(option1, option2)
   }
+
+const test1 = ['🟢', '🔴', '🟢', '🟢', '🟢']
+const test2 = ['🔴', '🔴', '🟢', '🟢', '🔴']
+const test3 = ['🟢', '🔴', '🟢', '🔴', '🟢']
+const test4 = ['🔴', '🔴', '🔴']
+const test5 = ['🟢', '🔴', '🔴', '🟢', '🔴']
+const test6 = ['🔴', '🔴', '🟢', '🔴', '🟢']
+
+
+console.log(adjustLights(test1)) // 1
+console.log(adjustLights(test2)) // 2
+console.log(adjustLights(test3)) // 0
+console.log(adjustLights(test4)) // 1
+console.log(adjustLights(test5)) // 2
+console.log(adjustLights(test6)) // 1
