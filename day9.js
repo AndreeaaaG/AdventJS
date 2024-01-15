8a43ff1f15d7365a9a893ae0946cb25947ce59ee
AndreeaaaG <andreea.gavrila@epg.ro>
2024-01-15 13:09:57 +0200

    Do it in O(n) with only 1 for

--- day9.js
+++ day9.js
@@ -1,26 +1,25 @@
 function adjustLights(lights) {
-    let option1 = 0
-    const lightsCopy1 = [...lights]
-    const lightsCopy2 = [...lights]
-    let option2 = 0
+  const lightsCopy = [...lights];
+  let option1 = 0;
+  let option2 = 0;
   
-    for (let i = 0; i < lights.length - 1; i++) {
-      if (lightsCopy1[i] === lightsCopy1[i + 1]) {
-        option1++
-        lightsCopy1[i + 1] = lightsCopy1[i] === '🔴' ? '🟢' : '🔴'
-      }
-    }
-  
-    for (let i = lights.length - 1; i > 0; i--) {
-      if (lightsCopy2[i] === lightsCopy2[i - 1]) {
-        option2++
-        lightsCopy2[i - 1] = lightsCopy2[i] === '🔴' ? '🟢' : '🔴'
-      }
-    }
-  
-    return Math.min(option1, option2)
-  }
+  for (let i = 0; i < lights.length - 1; i++) {
 
+      if (lightsCopy[i] === lightsCopy[i + 1]) {
+          option1++;
+          lightsCopy[i + 1] = lightsCopy[i]  === '🔴' ? '🟢' : '🔴';
+      }
+      // Check the opposite side as well in the same loop
+      if (lights[lights.length - 1 - i] === lights[lights.length - 2 - i]) {
+          option2++;
+          lights[lights.length - 2 - i] = lights[lights.length - 1 - i] === '🔴' ? '🟢' : '🔴';
+      }
+  }
+  
+  return Math.min(option1, option2);
+}
+
+const lights = ['🔴', '🔴', '🔴', '🟢', '🟢', '🔴', '🔴'];
 const test1 = ['🟢', '🔴', '🟢', '🟢', '🟢']
 const test2 = ['🔴', '🔴', '🟢', '🟢', '🔴']
 const test3 = ['🟢', '🔴', '🟢', '🔴', '🟢']
@@ -28,7 +27,7 @@ const test4 = ['🔴', '🔴', '🔴']
 const test5 = ['🟢', '🔴', '🔴', '🟢', '🔴']
 const test6 = ['🔴', '🔴', '🟢', '🔴', '🟢']
 
-
+console.log(adjustLights(lights))  //  3
 console.log(adjustLights(test1)) // 1
 console.log(adjustLights(test2)) // 2
 console.log(adjustLights(test3)) // 0
